30a3f7df65ffa35ebeacc434d54a8e3ed9b3f0ba
Andrisa dos Santos Silva <148649363+Andrisa-S@users.noreply.github.com>
2025-04-28 10:51:53 -0300

    Add files via upload

---
+++ MySQL/Aula28-04.sql
@@ -0,0 +1,32 @@
+#Mostra todas as bases de dados do SGBD
+show databases;
+drop database mydb;
+
+#Invoca/chama um banco de dados específico
+use performance_schema;
+
+#Mostra as tabelas do banco de dados invocado ou em uso
+show tables;
+
+#Criar a base de dados desejada - consulta
+
+#Invocar a base de dados criada
+
+#Criar as tabelas
+	#para cada tabela:
+		#Criar atributo com seu tipo
+        #Definir restrições
+			#atributo
+            #chave primária
+            #chave estrangeira
+            
+#--Exercício sobre a base criada...
+
+show databases;
+use mydb;
+show tables;
+
+-- na tabela medico preciso alterar o tamanho do nome... de 45 caracteres para 100 caracteres
+-- na tabela consulta preciso adicionar um atributo chamado convênio
+
+-- usar o alter table
